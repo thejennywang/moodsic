ce91a4e1f2d7109b4fd62592d20441e305e37c3b
Jeremy Fox <foxjerem@gmail.com>
2014-08-06 16:44:01 +0100

    First test passed

--- spec/MoodsicSpec.js
+++ spec/MoodsicSpec.js
@@ -3,37 +3,37 @@ describe("Moodsic", function() {
 	describe('selecting a playlist', function() {
 
 		beforeEach(function(){
-			moodsic = new Moodsic;
+			moodsic = new Moodsic();
 		});
 
 		it('should return a playlist for snow', function() {
 			var weatherDesc = 'Patchy light snow in area with thunder';
-			expect(moodsic.returnPlaylistUrlFrom(weatherDesc)).toEq(moodsic.MOODS.snow);
+			expect(moodsic.returnPlaylistUrlFrom(weatherDesc)).toEqual(moodsic.MOODS.snow);
 		});
 	
 		it('should return a playlist for rain', function() {
 			var weatherDesc = 'Torrential rain shower';
-			expect(moodsic.returnPlaylistUrlFrom(weatherDesc)).toEq(moodsic.MOODS.rain);
+			expect(moodsic.returnPlaylistUrlFrom(weatherDesc)).toEqual(moodsic.MOODS.rain);
 		});
 
 		it('should return a playlist for sunny', function() {
 			var weatherDesc = 'Clear/Sunny';
-			expect(moodsic.returnPlaylistUrlFrom(weatherDesc)).toEq(moodsic.MOODS.sunny);
+			expect(moodsic.returnPlaylistUrlFrom(weatherDesc)).toEqual(moodsic.MOODS.sunny);
 		});
 
 		it('should return a playlist for drizzle', function() {
 			var weatherDesc = 'Light drizzle';
-			expect(moodsic.returnPlaylistUrlFrom(weatherDesc)).toEq(moodsic.MOODS.drizzle);
+			expect(moodsic.returnPlaylistUrlFrom(weatherDesc)).toEqual(moodsic.MOODS.drizzle);
 		});
 
 		it('should return a playlist for cloudy', function() {
 			var weatherDesc = 'Overcast';
-			expect(moodsic.returnPlaylistUrlFrom(weatherDesc)).toEq(moodsic.MOODS.cloudy);
+			expect(moodsic.returnPlaylistUrlFrom(weatherDesc)).toEqual(moodsic.MOODS.cloudy);
 		});
 
 		it('should return a default playlist', function() {
 			var weatherDesc = 'sdfgfdh';
-			expect(moodsic.returnPlaylistUrlFrom(weatherDesc)).toEq(moodsic.MOODS.other);
+			expect(moodsic.returnPlaylistUrlFrom(weatherDesc)).toEqual(moodsic.MOODS.other);
 		});
 
 	});
